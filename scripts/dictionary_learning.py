7770f1a9ba8c75fac2e5a12679a933c68d91c25a
Cesar Caiafa <ccaiafa@Cesars-MacBook-Pro.local>
2023-08-04 13:36:34 -0300

    change to full dictionary learning

--- scripts/dictionary_learning.py
+++ scripts/dictionary_learning.py
@@ -8,9 +8,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from configs.constants import Constants
 
-#from executor.dictionary_trainer import Dictionary_Trainer
+from executor.dictionary_trainer import Dictionary_Trainer
 
-from executor.kronecker_dictionary_trainer import Dictionary_Trainer
+#from executor.kronecker_dictionary_trainer import Dictionary_Trainer
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
