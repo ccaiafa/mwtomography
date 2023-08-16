f846d2339a99e86623cfe1c9cd00e426f7d418e6
Cesar Caiafa <ccaiafa@Cesars-MacBook-Pro.local>
2023-08-16 12:38:42 -0300

    save_dict script added

---
+++ scripts/save_dict.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import hdf5storage
+import sys
+from os.path import join as pjoin
+
+ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.file_manager import FileManager
+
+
+if __name__ == "__main__":
+    filename_dict_learner = os.path.join(
+        ROOT_PATH + "/dictionary/trained_dict_epoch__patch_64x64_epoch_4_batch_size_125000_ncomps_1024.pkl")
+    aux = FileManager.load(filename_dict_learner)
+
+    filename_dict = os.path.join(
+        ROOT_PATH + "/dictionary/trained_dict_epoch__patch_64x64_epoch_4_batch_size_125000_ncomps_1024_dict.pkl")
+    FileManager.save(aux.components_, filename_dict)
+
+
