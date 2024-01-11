1fd23295d55e7f32feef7d623d0451bfd0df0ce6
Cesar Caiafa <ccaiafa@Cesars-MacBook-Pro.local>
2024-01-11 11:12:35 +0900

    added setup files

---
+++ mwtomography.py
@@ -0,0 +1,6 @@
+__version__ = '0.1'
+
+
+def hello_world():
+    print("This is my first pip package!")
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from mwtomography import __version__
+
+setup(
+    name='mwtomography',
+    version=__version__,
+
+    url='https://github.com/ccaiafa/mwtomography',
+    author='Cesar Caiafa',
+    author_email='ccaiafa@gmail.com',
+
+    py_modules=['mwtomography'],
+)
