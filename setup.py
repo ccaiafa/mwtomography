d9c089c2caf0308e5abd03fed024f0a38f1cdd99
Cesar Caiafa <ccaiafa@cesars-mbp.ad18.riken.go.jp>
2024-01-29 14:40:39 +0900

    fix package_data in setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import os
-import glob
+
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -13,20 +13,20 @@ def read(fname):
 from setuptools import setup, find_packages
 
 setup(
-    name = "mwtomography",
-    version = "1.0.0",
-    author = "Cesar Caiafa",
-    author_email = "ccaiafa@gmail.com",
-    description = ("MicroWave Tomography tools"),
-    license = "MIT",
-    keywords = "mwtomography microwaves tomography",
-    packages = find_packages(),
-    long_description = read('README.md'),
+    name="mwtomography",
+    version="1.0.0",
+    author="Cesar Caiafa",
+    author_email="ccaiafa@gmail.com",
+    description=("MicroWave Tomography tools"),
+    license="MIT",
+    keywords="mwtomography microwaves tomography",
+    packages=find_packages(),
+    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
     ],
     zip_safe=False,
-    data_files=glob.glob('mwtomography/configs/**')
+    package_data={'': ['*.json']}
 )
