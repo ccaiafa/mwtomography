817b21cfef075d9b5ff3af41e000efa49338cc7e
Cesar Caiafa <ccaiafa@cesars-mbp.ad18.riken.go.jp>
2024-01-29 14:17:02 +0900

    check if basic parameters file exists

--- mwtomography/configs/constants.py
+++ mwtomography/configs/constants.py
@@ -12,16 +12,8 @@ class Constants:
     @staticmethod
     def get_basic_parameters():
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        with open(os.path.join(dir_path, "basic_parameters.json"), "r") as f:
-            basic_parameters = json.load(f)
-            #basic_parameters["physics"]["impedance_of_free_space"] = 120 * pi  # eta_0
-
-            #basic_parameters["physics"]["k_0"] = 2 * pi / basic_parameters["physics"]["wavelength"]
-            #basic_parameters["physics"]["omega"] = basic_parameters["physics"]["k_0"] * basic_parameters["physics"]["speed_of_light"]
-            
-            #basic_parameters["images"]["step_size"] = 2 * basic_parameters["images"]["max_diameter"] / (basic_parameters["images"]["no_of_pixels"] - 1)
-            #basic_parameters["images"]["cell_area"] = basic_parameters["images"]["step_size"]^2
-            
-            #basic_parameters["physics"]["gamma"] = basic_parameters["physics"]["omega"] * basic_parameters["physics"]["vacuum_permittivity"] * basic_parameters["images"]["cell_area"]
+        if os.path.isfile(os.path.join(dir_path, "basic_parameters.json")):
+            with open(os.path.join(dir_path, "basic_parameters.json"), "r") as f:
+                basic_parameters = json.load(f)
 
             return basic_parameters
