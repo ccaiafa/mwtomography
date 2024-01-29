ce212b020e947237f9be8677acabb83ec1a0c25e
Cesar Caiafa <ccaiafa@cesars-mbp.ad18.riken.go.jp>
2024-01-29 14:55:26 +0900

    constans.py changed

--- build/lib/mwtomography/configs/constants.py
+++ build/lib/mwtomography/configs/constants.py
@@ -12,8 +12,7 @@ class Constants:
     @staticmethod
     def get_basic_parameters():
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        if os.path.isfile(os.path.join(dir_path, "basic_parameters.json")):
-            with open(os.path.join(dir_path, "basic_parameters.json"), "r") as f:
-                basic_parameters = json.load(f)
+        with open(os.path.join(dir_path, "basic_parameters.json"), "r") as f:
+            basic_parameters = json.load(f)
 
-            return basic_parameters
+        return basic_parameters
